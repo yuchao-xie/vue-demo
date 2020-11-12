711b838cbc07f4ffeb760b8ed858708c1bb83e07
xieyuchao <xieyuchao@eefung.com>
2020-11-12 09:57:06 +0800

    使用d3画力导向图

--- src/module/home.vue
+++ src/module/home.vue
@@ -1,24 +1,49 @@
 <style lang="less" rel="stylesheet/less">
-  @import "./home.less";
+@import "./home.less";
 </style>
 
 <template>
-  <div class="home-container">123456</div>
+  <div id="map">
+    <!-- <p></p>
+    <p></p>
+    <p class='myP'></p>
+    <p></p> -->
+  </div>
 </template>
 
 <script>
+import knowledgeMapD3 from "../utils/knowledge-map-d3.js";
+
 export default {
   props: {},
   data() {
-    return {};
+    return {
+      data: {
+        nodes: [
+          { id: "人物1", name: "人物1" },
+          { id: "人物2", name: "人物2" },
+          { id: "人物3", name: "人物3" },
+        ],
+        links: [
+          { target: "人物1", source: "人物2" },
+          { target: "人物1", source: "人物3" },
+        ],
+      },
+    };
   },
   created() {},
   watch: {},
   components: {},
-  mounted() {},
+  mounted() {
+    this.renderMap();
+  },
   computed: {},
-  methods: {},
+  methods: {
+    renderMap() {
+      knowledgeMapD3.renderChart("#map", this.data);
+    },
+  },
   beforeDestroy() {},
-  destroyed() {}
+  destroyed() {},
 };
 </script>
