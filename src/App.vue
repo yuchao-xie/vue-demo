56720f3dddff9e9074268ce075b828ea83747359
xieyuchao <xieyuchao@eefung.com>
2021-01-05 16:08:55 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="app">
         <router-view></router-view>
-    </div>
+    </div> 
 </template>
 
 <script>
@@ -16,7 +16,7 @@ export default {
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
         text-align: center;
-        color: #2c3e50;
+        color: #2c3e51;
         margin-top: 60px;
     } */
 
